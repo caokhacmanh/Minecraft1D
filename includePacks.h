ff87a78a2d6cc583b5dc2fcbdd64965360239ea1
caokhacmanh <caokhacmanh2018@gmail.com>
2021-12-01 22:11:11 +0700

    .

---
+++ includePacks.h
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+#include <time.h>
+#include <unistd.h>
+#include <conio.h>
---
+++ laucher.cpp
@@ -0,0 +1,6 @@
+#include ".\\includePacks.h"
+using namespace std;
+
+int main(){
+    
+}
